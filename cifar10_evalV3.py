d05904b70b8abc7e2c38574a8b7ffac983f917fc
zhuangzheng <z283@qq.com>
2018-03-15 23:33:52 +0800

    Add files via upload

--- cifar10_evalV3.py
+++ cifar10_evalV3.py
@@ -1,4 +1,4 @@
-# Copyright 2015 Google Inc. All Rights Reserved.
+# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+
 """Evaluation for CIFAR-10.
 Accuracy:
 cifar10_train.py achieves 83.0% accuracy after 100K steps (256 epochs
@@ -28,20 +29,23 @@ http://tensorflow.org/tutorials/deep_cnn/
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+
 from datetime import datetime
 import math
 import time
-import tensorflow.python.platform
-from tensorflow.python.platform import gfile
+
 import numpy as np
 import tensorflow as tf
-from tensorflow.models.image.cifar10 import cifar10
+
+import cifar10V3
+
 FLAGS = tf.app.flags.FLAGS
-tf.app.flags.DEFINE_string('eval_dir', '/tmp/cifar10_eval',
+
+tf.app.flags.DEFINE_string('eval_dir', '/tmp/cifar10_evalV3',
                            """Directory where to write event logs.""")
 tf.app.flags.DEFINE_string('eval_data', 'test',
-                           """Either 'test' or 'train_eval'.""")
-tf.app.flags.DEFINE_string('checkpoint_dir', '/tmp/cifar10_train',
+                           """Either 'test' or 'train_evalV3'.""")
+tf.app.flags.DEFINE_string('checkpoint_dir', '/tmp/cifar10_trainV3',
                            """Directory where to read model checkpoints.""")
 tf.app.flags.DEFINE_integer('eval_interval_secs', 60 * 5,
                             """How often to run the eval.""")
@@ -49,6 +53,8 @@ tf.app.flags.DEFINE_integer('num_examples', 10000,
                             """Number of examples to run.""")
 tf.app.flags.DEFINE_boolean('run_once', False,
                          """Whether to run eval only once.""")
+
+
 def eval_once(saver, summary_writer, top_k_op, summary_op):
   """Run Eval once.
   Args:
@@ -69,6 +75,7 @@ def eval_once(saver, summary_writer, top_k_op, summary_op):
     else:
       print('No checkpoint file found')
       return
+
     # Start the queue runners.
     coord = tf.train.Coordinator()
     try:
@@ -76,6 +83,7 @@ def eval_once(saver, summary_writer, top_k_op, summary_op):
       for qr in tf.get_collection(tf.GraphKeys.QUEUE_RUNNERS):
         threads.extend(qr.create_threads(sess, coord=coord, daemon=True,
                                          start=True))
+
       num_iter = int(math.ceil(FLAGS.num_examples / FLAGS.batch_size))
       true_count = 0  # Counts the number of correct predictions.
       total_sample_count = num_iter * FLAGS.batch_size
@@ -84,48 +92,61 @@ def eval_once(saver, summary_writer, top_k_op, summary_op):
         predictions = sess.run([top_k_op])
         true_count += np.sum(predictions)
         step += 1
+
       # Compute precision @ 1.
       precision = true_count / total_sample_count
       print('%s: precision @ 1 = %.3f' % (datetime.now(), precision))
+
       summary = tf.Summary()
       summary.ParseFromString(sess.run(summary_op))
       summary.value.add(tag='Precision @ 1', simple_value=precision)
       summary_writer.add_summary(summary, global_step)
     except Exception as e:  # pylint: disable=broad-except
       coord.request_stop(e)
+
     coord.request_stop()
     coord.join(threads, stop_grace_period_secs=10)
+
+
 def evaluate():
   """Eval CIFAR-10 for a number of steps."""
-  with tf.Graph().as_default():
+  with tf.Graph().as_default() as g:
     # Get images and labels for CIFAR-10.
     eval_data = FLAGS.eval_data == 'test'
-    images, labels = cifar10.inputs(eval_data=eval_data)
+    images, labels = cifar10V3.inputs(eval_data=eval_data)
+
     # Build a Graph that computes the logits predictions from the
     # inference model.
-    logits = cifar10.inference(images)
+    logits = cifar10V3.inference(images)
+
     # Calculate predictions.
     top_k_op = tf.nn.in_top_k(logits, labels, 1)
+
     # Restore the moving average version of the learned variables for eval.
     variable_averages = tf.train.ExponentialMovingAverage(
-        cifar10.MOVING_AVERAGE_DECAY)
+        cifar10V3.MOVING_AVERAGE_DECAY)
     variables_to_restore = variable_averages.variables_to_restore()
     saver = tf.train.Saver(variables_to_restore)
+
     # Build the summary operation based on the TF collection of Summaries.
-    summary_op = tf.merge_all_summaries()
-    graph_def = tf.get_default_graph().as_graph_def()
-    summary_writer = tf.train.SummaryWriter(FLAGS.eval_dir,
-                                            graph_def=graph_def)
+    summary_op = tf.summary.merge_all()
+
+    summary_writer = tf.summary.FileWriter(FLAGS.eval_dir, g)
+
     while True:
       eval_once(saver, summary_writer, top_k_op, summary_op)
       if FLAGS.run_once:
         break
       time.sleep(FLAGS.eval_interval_secs)
+
+
 def main(argv=None):  # pylint: disable=unused-argument
-  cifar10.maybe_download_and_extract()
-  if gfile.Exists(FLAGS.eval_dir):
-    gfile.DeleteRecursively(FLAGS.eval_dir)
-  gfile.MakeDirs(FLAGS.eval_dir)
+  cifar10V3.maybe_download_and_extract()
+  if tf.gfile.Exists(FLAGS.eval_dir):
+    tf.gfile.DeleteRecursively(FLAGS.eval_dir)
+  tf.gfile.MakeDirs(FLAGS.eval_dir)
   evaluate()
+
+
 if __name__ == '__main__':
   tf.app.run()
--- cifar10_trainV3.py
+++ cifar10_trainV3.py
@@ -1,4 +1,4 @@
-# Copyright 2015 Google Inc. All Rights Reserved.
+# Copyright 2015 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+
 """A binary to train CIFAR-10 using a single GPU.
 Accuracy:
 cifar10_train.py achieves ~86% accuracy after 100K steps (256 epochs of
@@ -29,76 +30,93 @@ http://tensorflow.org/tutorials/deep_cnn/
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+
 from datetime import datetime
-import os.path
 import time
-import tensorflow.python.platform
-from tensorflow.python.platform import gfile
-import numpy as np
-from six.moves import xrange  # pylint: disable=redefined-builtin
+
 import tensorflow as tf
-from tensorflow.models.image.cifar10 import cifar10
+
+import cifar10V3
+
 FLAGS = tf.app.flags.FLAGS
-tf.app.flags.DEFINE_string('train_dir', '/tmp/cifar10_train',
+
+tf.app.flags.DEFINE_string('train_dir', '/tmp/cifar10_trainV3',
                            """Directory where to write event logs """
                            """and checkpoint.""")
 tf.app.flags.DEFINE_integer('max_steps', 1000000,
                             """Number of batches to run.""")
 tf.app.flags.DEFINE_boolean('log_device_placement', False,
                             """Whether to log device placement.""")
+tf.app.flags.DEFINE_integer('log_frequency', 10,
+                            """How often to log results to the console.""")
+
+
 def train():
   """Train CIFAR-10 for a number of steps."""
   with tf.Graph().as_default():
-    global_step = tf.Variable(0, trainable=False)
+    global_step = tf.train.get_or_create_global_step()
+
     # Get images and labels for CIFAR-10.
-    images, labels = cifar10.distorted_inputs()
+    # Force input pipeline to CPU:0 to avoid operations sometimes ending up on
+    # GPU and resulting in a slow down.
+    with tf.device('/cpu:0'):
+      images, labels = cifar10V3.distorted_inputs()
+
     # Build a Graph that computes the logits predictions from the
     # inference model.
-    logits = cifar10.inference(images)
+    logits = cifar10V3.inference(images)
+
     # Calculate loss.
-    loss = cifar10.loss(logits, labels)
+    loss = cifar10V3.loss(logits, labels)
+
     # Build a Graph that trains the model with one batch of examples and
     # updates the model parameters.
-    train_op = cifar10.train(loss, global_step)
-    # Create a saver.
-    saver = tf.train.Saver(tf.all_variables())
-    # Build the summary operation based on the TF collection of Summaries.
-    summary_op = tf.merge_all_summaries()
-    # Build an initialization operation to run below.
-    init = tf.initialize_all_variables()
-    # Start running operations on the Graph.
-    sess = tf.Session(config=tf.ConfigProto(
-        log_device_placement=FLAGS.log_device_placement))
-    sess.run(init)
-    # Start the queue runners.
-    tf.train.start_queue_runners(sess=sess)
-    summary_writer = tf.train.SummaryWriter(FLAGS.train_dir,
-                                            graph_def=sess.graph_def)
-    for step in xrange(FLAGS.max_steps):
-      start_time = time.time()
-      _, loss_value = sess.run([train_op, loss])
-      duration = time.time() - start_time
-      assert not np.isnan(loss_value), 'Model diverged with loss = NaN'
-      if step % 10 == 0:
-        num_examples_per_step = FLAGS.batch_size
-        examples_per_sec = num_examples_per_step / duration
-        sec_per_batch = float(duration)
-        format_str = ('%s: step %d, loss = %.2f (%.1f examples/sec; %.3f '
-                      'sec/batch)')
-        print (format_str % (datetime.now(), step, loss_value,
-                             examples_per_sec, sec_per_batch))
-      if step % 100 == 0:
-        summary_str = sess.run(summary_op)
-        summary_writer.add_summary(summary_str, step)
-      # Save the model checkpoint periodically.
-      if step % 1000 == 0 or (step + 1) == FLAGS.max_steps:
-        checkpoint_path = os.path.join(FLAGS.train_dir, 'model.ckpt')
-        saver.save(sess, checkpoint_path, global_step=step)
+    train_op = cifar10V3.train(loss, global_step)
+
+    class _LoggerHook(tf.train.SessionRunHook):
+      """Logs loss and runtime."""
+
+      def begin(self):
+        self._step = -1
+        self._start_time = time.time()
+
+      def before_run(self, run_context):
+        self._step += 1
+        return tf.train.SessionRunArgs(loss)  # Asks for loss value.
+
+      def after_run(self, run_context, run_values):
+        if self._step % FLAGS.log_frequency == 0:
+          current_time = time.time()
+          duration = current_time - self._start_time
+          self._start_time = current_time
+
+          loss_value = run_values.results
+          examples_per_sec = FLAGS.log_frequency * FLAGS.batch_size / duration
+          sec_per_batch = float(duration / FLAGS.log_frequency)
+
+          format_str = ('%s: step %d, loss = %.2f (%.1f examples/sec; %.3f '
+                        'sec/batch)')
+          print (format_str % (datetime.now(), self._step, loss_value,
+                               examples_per_sec, sec_per_batch))
+
+    with tf.train.MonitoredTrainingSession(
+        checkpoint_dir=FLAGS.train_dir,
+        hooks=[tf.train.StopAtStepHook(last_step=FLAGS.max_steps),
+               tf.train.NanTensorHook(loss),
+               _LoggerHook()],
+        config=tf.ConfigProto(
+            log_device_placement=FLAGS.log_device_placement)) as mon_sess:
+      while not mon_sess.should_stop():
+        mon_sess.run(train_op)
+
+
 def main(argv=None):  # pylint: disable=unused-argument
-  cifar10.maybe_download_and_extract()
-  if gfile.Exists(FLAGS.train_dir):
-    gfile.DeleteRecursively(FLAGS.train_dir)
-  gfile.MakeDirs(FLAGS.train_dir)
+  cifar10V3.maybe_download_and_extract()
+  if tf.gfile.Exists(FLAGS.train_dir):
+    tf.gfile.DeleteRecursively(FLAGS.train_dir)
+  tf.gfile.MakeDirs(FLAGS.train_dir)
   train()
+
+
 if __name__ == '__main__':
   tf.app.run()
